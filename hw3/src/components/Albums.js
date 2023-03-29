ca34ecd99a521760792fd532c04b8d092d78eb80
= <=>
2023-03-29 23:10:30 +0300

    hw task 1.1.a

--- hw3/src/components/Albums.js
+++ hw3/src/components/Albums.js
@@ -5,19 +5,19 @@ export class Albums extends React.Component {
   state = {
     albums: [],
   };
-
+  
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/albums")
-      .then((res) => res.json())
-      .then((albums) => this.setState({ albums }));
+    .then((res) => res.json())
+    .then((albums) => this.setState({ albums }));
   }
-
-  render() {
-    return (
-      <div>
-        {albums.map((album) => (
+  
+    render() {
+      return (
+        <div>
+        {this.state.albums.map((album) => (
           <div key={album.id}>
-            {album.id} - {album.title}
+            {album.userId} - {album.id} - {album.title}
           </div>
         ))}
       </div>
