854b231fb6f5a408aff267f7a7f247849708e290
= <=>
2023-04-02 11:41:23 +0300

    task 1.2* is to perform

--- hw3/src/App.js
+++ hw3/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Albums } from "./components/Task1/Albums";
+import { Form } from "./components/Task1/GetAlbumsByInput";
 
 export default class App extends React.Component {
   render() {
     return (
-        <Albums />
+      <>
+        {/* <Albums /> */}
+        <Form />
+      </>
     );
   }
 }
--- hw3/src/components/Task1/Context.js
+++ hw3/src/components/Task1/Context.js
@@ -6,12 +6,20 @@ export class DataProvider extends React.Component {
   state = {
     error: null,
     albums: [],
+    albumsToShow: [],
+    albumsNum: 0,
   };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/albums")
       .then((res) => res.json())
-      .then((albums) => this.setState({ albums }))
+      .then((albums) =>
+        this.setState({
+          albums: albums,
+          albumsToShow: albums.slice(),
+          albumsNum: albums.length,
+        })
+      )
       .catch((error) => {
         this.setState({ error });
         console.log({ error });
@@ -19,13 +27,24 @@ export class DataProvider extends React.Component {
       });
   }
 
+  handleChange = (inputNum) => {
+    const { albums } = this.state;
+    const albumsToShow = albums.slice(0, inputNum);
+
+    this.setState({
+      albumsToShow,
+      albumsNum: inputNum
+    });
+  };
+
   render() {
+    console.log(this.context);
     const { error } = this.state;
     if (error) {
       return <div>Ошибка: {error.message}</div>;
     } else {
       return (
-        <DataContext.Provider value={this.state}>
+        <DataContext.Provider value={{...this.state, handleChange: this.handleChange}}>
           {this.props.children}
         </DataContext.Provider>
       );
