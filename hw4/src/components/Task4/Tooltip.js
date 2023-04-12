3dcce1a429976d4262fd9d7e09433d04e3aee70f
= <=>
2023-04-12 22:15:19 +0300

    task 4 for correction

--- hw4/src/components/Task4/Tooltip.js
+++ hw4/src/components/Task4/Tooltip.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import ProductTooltip from "./ProductTooltip";
+import ProductCardList from "./ProductCardList";
 
 const Tooltip = ({ renderTooltip, content }) => {
   const [position, setPosition] = useState({ tooltipX: 0, tooltipY: 0 });
   const [isTooltipShown, setIsTooltipShown] = useState(false);
 
+  const { title, price, rating } = content;
+
   const toggleTooltip = () => {
-    setIsTooltipShown(prev => !prev);
+    setIsTooltipShown((prev) => !prev);
   };
 
   const handleMouseEnter = (event) => {
-    setIsTooltipShown(isTooltipShown);
+    setIsTooltipShown(!isTooltipShown);
     console.log(!isTooltipShown);
   };
 
@@ -24,10 +27,18 @@ const Tooltip = ({ renderTooltip, content }) => {
   };
 
   return (
-    <div onMouseEnter={handleMouseEnter}
+    <div
+      className="tooltip"
+      style={{ left: position.tooltipX, top: position.tooltipY }}
+      onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onMouseMove={handleMouseMove}
     >
+      <h4 className="tooltip_title">{title}</h4>
+      <p className="tooltip_option">{`Price: ${price} $`}</p>
+      <p className="tooltip_option">
+        {`Rating: ${rating.rate}`} {`Count: ${rating.count}`}
+      </p>
       {renderTooltip(position, content)}
     </div>
   );
